d2985df0af6d778248f37181f7a532d519079cc8
Julian Carpintero <juliancarpintero04@gmail.com>
2022-09-27 10:09:56 -0300

    Agregando mock e interface

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,27 @@
+import {Task} from "./Task"
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar Argentina Programa",
+        day: "15 de Enero a las 23:59",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Terminar Curso de masajes",
+        day: "17 de marzo a las 17:00",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Terminar Curso de robotica e impresion 3D",
+        day: "9 de diciembre a las 21:00",
+        reminder: true
+    },
+    {
+        id: 4,
+        text: "Terminar 5to de secundaria tecnica",
+        day: "18 de noviembre a las 12:30",
+        reminder: true
+    },
+]
